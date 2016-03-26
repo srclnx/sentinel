0ede423fdb9ac8ed83d9e2d68f197f53cd901adf
Ray Hayes <ray.hayes@outlook.com>
2016-03-26 02:22:55 +0000

    FxCop suggested nLog -> NLog rename for assembly

--- nLog1Tester/Properties/AssemblyInfo.cs
+++ nLog1Tester/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nLog1Tester")]
+[assembly: AssemblyTitle("NLog1Tester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("nLog1Tester")]
+[assembly: AssemblyProduct("NLog1Tester")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- nLog4Tester/Properties/AssemblyInfo.cs
+++ nLog4Tester/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nLog4Tester")]
+[assembly: AssemblyTitle("NLog4Tester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("nLog4Tester")]
+[assembly: AssemblyProduct("NLog4Tester")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
