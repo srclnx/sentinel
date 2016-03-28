75402aac5609d0139fe90115722f3f24b5df8a9b
Ray Hayes <ray.hayes@outlook.com>
2016-03-28 13:02:57 +0100

    Added the CreateInstaller.ps1 bootstrapper.

---
+++ Sentinel/CreateInstaller.ps1
@@ -0,0 +1,11 @@
+﻿$tempDirectoryName = ".\__squirrel_temp__"
+
+if ( $DTE -eq $null ) {
+	echo "Run this from the NuGet Package Console inside VS"
+	exit 1
+}
+
+mkdir -Path $tempDirectoryName
+rm -Recurse -Force "$tempDirectoryName\*.nupkg"
+NuGet pack .\sentinel.sln -outputDirectory $tempDirectoryName
+ls "$tempDirectoryName\*.nupkg" | % { Squirrel --releasify $_ -p .\packages }
